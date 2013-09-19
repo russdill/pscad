0b264199dd0d93b7744468334b9c04b9680f0384
Russ Dill <Russ.Dill@gmail.com>
2013-09-19 10:15:42 -0700

    mlp update

--- mlp.py
+++ mlp.py
@@ -27,7 +27,8 @@ defaults = {
     'grid'      :   "0.5",
     'clearance' :   "0.15",
     'mask' :        "2.5 mil",
-    'silk' :        "0.2"
+    'silk' :        "0.2",
+    'start_pin' :   "1"
 }
 
 def mlp_pad(m):
@@ -45,24 +46,39 @@ def mlp_pad(m):
     else:
         return pad
 
-def mlp_row(m, n):
-    return pscad.down(m.width / D(2)) + pscad.row(mlp_pad(m), m.pitch, n, center=True)
+def mlp_row(m, n, dist):
+    return pscad.down(dist / 2) + pscad.row(mlp_pad(m), m.pitch, n, center=True)
 
 def part(m):
     m = pscad.wrapper(defaults.items() + m.items())
 
+    if 'pads_x' in m:
+        pads_x = m.pads_x
+    elif m.quad:
+        pads_x = m.n / 4
+    else:
+        pads_x = 0
+    pads_y = m.n / 2 - pads_x
+
+    if 'width' in m:
+        width = m.width
+    else:
+        width = m.height
+
     thermal_size = (m.thermal_x, m.thermal_y)
-    row = mlp_row(m, m.n / (4 if m.quad else 2))
+    row_y = mlp_row(m, pads_y, m.height)
 
     if m.quad:
+        row_x = mlp_row(m, pads_x, width)
         pads = (
-            row, pscad.rotate(90) + row,
-            pscad.rotate(180) + row, pscad.rotate(270) + row
+            row_y, pscad.rotate(90) + row_x,
+            pscad.rotate(180) + row_y, pscad.rotate(270) + row_x
         )
     else:
-        pads = row, pscad.rotate(180) + row
+        pads = row_y, pscad.rotate(180) + row_y
 
-    all = pscad.pad(itertools.count(1), m.clearance, m.mask) + (
+    pin_list = range(m.start_pin, m.n - m.start_pin + 3) + range(1, m.start_pin) + [int(m.n) + 1]
+    all = pscad.pad(itertools.cycle(pin_list), m.clearance, m.mask) + (
         pads,
         patterns.thermal_pad(patterns.indent_pad(thermal_size, m.indent),
                 thermal_size, m.paste_fraction, m.paste_max)
